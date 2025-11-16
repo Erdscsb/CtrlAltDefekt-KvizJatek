930c1c46bf6d2e0f497119895dc588d672c950d6
Recki15 <aticsati16@gmail.com>
2025-11-16 15:52:26 +0100

    Menü középre igazítva
    Középre igazítottam a főmenüt és állítottam a szélesség magasságon

--- kvizjatek-frontend/src/Pages/Home/HomePage.tsx
+++ kvizjatek-frontend/src/Pages/Home/HomePage.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import CenterStage from '../../Components/Layout/CenterStage';
-import GlassBackground from '../../Components/Layout/GlassBackground';
-import { MainMenu } from '../../Components/MainMenu';
+import React from "react";
+import CenterStage from "../../Components/Layout/CenterStage";
+import GlassBackground from "../../Components/Layout/GlassBackground";
+import { MainMenu } from "../../Components/MainMenu";
 
 const HomePage: React.FC = () => {
   return (
     <CenterStage>
-      <div style={{ width: '100%', maxWidth: 980 }}>
-        <div style={{ textAlign: 'center', marginBottom: 18 }}>
-          <img src={'../../assets/react.svg'} alt="Kvízjáték" height={56} />
+      <div className="page-wrap">
+        <div style={{ textAlign: "center", marginBottom: 18 }}>
+          <img src={"../../assets/logo.svg"} alt="Kvízjáték" height={56} />
         </div>
         <GlassBackground className="menu-surface">
           <MainMenu />
