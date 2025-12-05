f5eeb50c409625336cd80bd2291bce3a3044f677
Tusnádi Szabolcs <tusnadiszabolcs@gmail.com>
2025-12-05 08:19:12 +0100

    feat: added endpoint list to the console at the start of the backend

--- kvizjatek-backend/run.py
+++ kvizjatek-backend/run.py
@@ -11,5 +11,17 @@ def make_shell_context():
         'Question': Question, 'Result': Result, 'Topic': Topic
     }
 
+def list_routes():
+    """Listing all api endpoints"""
+    print("----------------------------------------------------------------")
+    print("Flask endpoints list")
+    print("----------------------------------------------------------------")
+    
+    # app.url_map tartalmazza az összes útvonalat
+    for rule in app.url_map.iter_rules():
+        methods = ','.join(rule.methods - set(['OPTIONS', 'HEAD']))
+        print(f"Endpoint: {rule.endpoint:<20} | Methods: {methods:<10} | URL: {rule.rule}")
+
 if __name__ == '__main__':
+    list_routes()
     app.run(debug=True)
